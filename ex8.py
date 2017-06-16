5b3a038e6a582e277269ed09231fb653e39ef50f
venesh31 <veneshbathula@gmail.com>
2017-06-16 13:18:08 +0000

    sachin

---
+++ ex8.py
@@ -0,0 +1,15 @@
+print """
+sachin is always a great player
+dravid was overshadowed by sachin
+ganguly created his own set of fans 
+"""
+months = "jan\nfeb\nmar\n"
+days = "mon,tues,wed"
+print months + days
+
+#formatter %r prints as it is raw does not take escape sequence 
+
+tabby= "I am 5'8\""
+print "tabby %r" %(tabby)
+ 
+
